ba2e7b8441bbe90c55625c0209b86cbcf83d902f
sqkysqnt <39272083+sqkysqnt@users.noreply.github.com>
2024-06-15 13:24:52 -0500

    Delete scripts directory

--- scripts/publisher.py
+++
@@ -1,9 +0,0 @@
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='test')
-
-channel.basic_publish(exchange='', routing_key='test', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
